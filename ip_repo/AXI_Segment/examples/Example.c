f69739e6e3abf2facbe2d6ca4249944f783135b0
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-01-19 18:19:41 +0100

    Add seven segment IP core

---
+++ ip_repo/AXI_Segment/examples/Example.c
@@ -0,0 +1,19 @@
+#include "xparameters.h"
+#include "axisegment.h"
+
+int main(void)
+{
+	AxiSegment Display;
+
+	if(AxiSegment_CfgInitialize(&Display, AxiSegment_LookupConfig(XPAR_AXI_SEGMENT_0_DEVICE_ID)) == XST_SUCCESS)
+	{
+		AxiSegment_Write(&Display, 0x18);
+	}
+
+	while(1)
+	{
+	}
+
+	return 0;
+}
+
