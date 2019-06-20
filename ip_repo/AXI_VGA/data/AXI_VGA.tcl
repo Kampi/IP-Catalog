34ec0cb1db8458651335f40a0d26d090d7a918e9
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-06-20 21:26:04 +0200

    Clean Repository, add VGA_Decimate for AXI VDMA

--- ip_repo/AXI_VGA/data/AXI_VGA.tcl
+++
@@ -1,6 +0,0 @@
-
-
-proc generate {drv_handle} {
-	xdefine_include_file $drv_handle "xparameters.h" "AxiVga" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR"
-	xdefine_config_file $drv_handle "AxiVga_g.c" "AxiVga" "DEVICE_ID" "C_BASEADDR"
-}
