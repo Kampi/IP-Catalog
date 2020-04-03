e312c8be83f40a5f629f3867f6aabd4568285aca
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2020-04-03 23:36:37 +0200

    Update 'VGA_Decimate' IP core from video project

--- ip_repo/VGA_Decimate/hdl/VGA_Decimate.vhd
+++ ip_repo/VGA_Decimate/hdl/VGA_Decimate.vhd
@@ -33,32 +33,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity VGA_Decimate is
-    Generic (   INPUT       : INTEGER := 24;
-                INPUT_SIZE  : INTEGER := 8;
+    Generic (   INPUT_SIZE  : INTEGER := 24;
+                COLOR_SIZE  : INTEGER := 8;
                 OUTPUT      : INTEGER := 16;
                 RED_END     : INTEGER := 4;
                 GREEN_END   : INTEGER := 5;
                 BLUE_END    : INTEGER := 4
                 );
     Port (  Enable   : in STD_LOGIC_VECTOR(0 downto 0);
-            Data_In  : in STD_LOGIC_VECTOR((INPUT - 1) downto 0);
+            Data_In  : in STD_LOGIC_VECTOR((INPUT_SIZE - 1) downto 0);
             Data_Out : out STD_LOGIC_VECTOR((OUTPUT - 1) downto 0)
             );
 end VGA_Decimate;
 
 architecture VGA_Decimate_Arch of VGA_Decimate is
 
-    signal Red      : STD_LOGIC_VECTOR((INPUT_SIZE - 1) downto 0);
-    signal Green    : STD_LOGIC_VECTOR((INPUT_SIZE - 1) downto 0);
-    signal Blue     : STD_LOGIC_VECTOR((INPUT_SIZE - 1) downto 0);
+    signal Red      : STD_LOGIC_VECTOR((COLOR_SIZE - 1) downto 0);
+    signal Green    : STD_LOGIC_VECTOR((COLOR_SIZE - 1) downto 0);
+    signal Blue     : STD_LOGIC_VECTOR((COLOR_SIZE - 1) downto 0);
 
     signal Data_Out_Int : STD_LOGIC_VECTOR((OUTPUT - 1) downto 0);
 
 begin
 
-    Red <= Data_In((INPUT_SIZE - 1) downto 0);
-    Green <= Data_In(((2 * INPUT_SIZE) - 1) downto INPUT_SIZE);
-    Blue <= Data_In(((3 * INPUT_SIZE) - 1) downto (2 * INPUT_SIZE));
+    Red <= Data_In((COLOR_SIZE - 1) downto 0);
+    Green <= Data_In(((2 * COLOR_SIZE) - 1) downto COLOR_SIZE);
+    Blue <= Data_In(((3 * COLOR_SIZE) - 1) downto (2 * COLOR_SIZE));
 
     Data_Out_Int <= Blue(BLUE_END downto 0) & Green(GREEN_END downto 0) & Red(RED_END downto 0);
     Data_Out <= Data_Out_Int and Enable;
--- ip_repo/VGA_Decimate/xgui/VGA_Decimate_v1_0.tcl
+++ ip_repo/VGA_Decimate/xgui/VGA_Decimate_v1_0.tcl
@@ -1,18 +1,18 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  set INPUT [ipgui::add_param $IPINST -name "INPUT"]
-  set_property tooltip {Input data width.} ${INPUT}
-  set INPUT_SIZE [ipgui::add_param $IPINST -name "INPUT_SIZE"]
-  set_property tooltip {Size of color components from input vector.} ${INPUT_SIZE}
-  set OUTPUT [ipgui::add_param $IPINST -name "OUTPUT"]
+  set INPUT_SIZE [ipgui::add_param $IPINST -name "INPUT_SIZE" -widget comboBox]
+  set_property tooltip {Input signal color components size.} ${INPUT_SIZE}
+  set OUTPUT [ipgui::add_param $IPINST -name "OUTPUT" -widget comboBox]
   set_property tooltip {Output data width.} ${OUTPUT}
   set RED_END [ipgui::add_param $IPINST -name "RED_END"]
-  set_property tooltip {Last bit position of color RED in output vector.} ${RED_END}
+  set_property tooltip {Last bit position of color RED used in output vector.} ${RED_END}
   set GREEN_END [ipgui::add_param $IPINST -name "GREEN_END"]
-  set_property tooltip {Last bit position of color GREEN in output vector.} ${GREEN_END}
+  set_property tooltip {Last bit position of color GREEN used in output vector.} ${GREEN_END}
   set BLUE_END [ipgui::add_param $IPINST -name "BLUE_END"]
-  set_property tooltip {Last bit position of color BLUE in output vector.} ${BLUE_END}
+  set_property tooltip {Last bit position of color BLUE used in output vector.} ${BLUE_END}
+  set COLOR_SIZE [ipgui::add_param $IPINST -name "COLOR_SIZE" -widget comboBox]
+  set_property tooltip {Input signal color components size.} ${COLOR_SIZE}
 
 }
 
@@ -25,21 +25,21 @@ proc validate_PARAM_VALUE.BLUE_END { PARAM_VALUE.BLUE_END } {
 	return true
 }
 
+proc update_PARAM_VALUE.COLOR_SIZE { PARAM_VALUE.COLOR_SIZE } {
+	# Procedure called to update COLOR_SIZE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.COLOR_SIZE { PARAM_VALUE.COLOR_SIZE } {
+	# Procedure called to validate COLOR_SIZE
+	return true
+}
+
 proc update_PARAM_VALUE.GREEN_END { PARAM_VALUE.GREEN_END } {
 	# Procedure called to update GREEN_END when any of the dependent parameters in the arguments change
 }
 
 proc validate_PARAM_VALUE.GREEN_END { PARAM_VALUE.GREEN_END } {
 	# Procedure called to validate GREEN_END
-	return true
-}
-
-proc update_PARAM_VALUE.INPUT { PARAM_VALUE.INPUT } {
-	# Procedure called to update INPUT when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.INPUT { PARAM_VALUE.INPUT } {
-	# Procedure called to validate INPUT
 	return true
 }
 
@@ -71,11 +71,6 @@ proc validate_PARAM_VALUE.RED_END { PARAM_VALUE.RED_END } {
 }
 
 
-proc update_MODELPARAM_VALUE.INPUT { MODELPARAM_VALUE.INPUT PARAM_VALUE.INPUT } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.INPUT}] ${MODELPARAM_VALUE.INPUT}
-}
-
 proc update_MODELPARAM_VALUE.OUTPUT { MODELPARAM_VALUE.OUTPUT PARAM_VALUE.OUTPUT } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.OUTPUT}] ${MODELPARAM_VALUE.OUTPUT}
@@ -99,5 +94,10 @@ proc update_MODELPARAM_VALUE.GREEN_END { MODELPARAM_VALUE.GREEN_END PARAM_VALUE.
 proc update_MODELPARAM_VALUE.BLUE_END { MODELPARAM_VALUE.BLUE_END PARAM_VALUE.BLUE_END } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.BLUE_END}] ${MODELPARAM_VALUE.BLUE_END}
+}
+
+proc update_MODELPARAM_VALUE.COLOR_SIZE { MODELPARAM_VALUE.COLOR_SIZE PARAM_VALUE.COLOR_SIZE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.COLOR_SIZE}] ${MODELPARAM_VALUE.COLOR_SIZE}
 }
 
